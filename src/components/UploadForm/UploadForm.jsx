ad5ab15527488a6090645ca0d1b6776b421d3c56
Gaurav <program.drgk@gmail.com>
2024-03-17 01:45:33 -0700

    Form validation, submission & navigate to Home DONE

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -2,15 +2,88 @@ import { useNavigate } from "react-router-dom";
 import "./UploadForm.scss";
 import PublishIcon from "../../assets/icons/publish.svg";
 import Button from "../../components/Button/Button";
+import { useState } from "react";
 
 function UploadForm() {
-  let navigate = useNavigate();
+  const navigate = useNavigate();
+
+  const [videoTitle, setVideoTitle] = useState("");
+  const [videoDescription, setVideoDescription] = useState("");
+
+  const handleVideoTitle = (event) => {
+    setVideoTitle(event.target.value);
+  };
+
+  const handleVideoDescription = (event) => {
+    setVideoDescription(event.target.value);
+  };
+
+  console.log(videoTitle);
+  console.log(videoDescription);
+
+  // to check if form data is valid
+
+  function isTitleValid() {
+    if (typeof videoTitle === "string" && videoTitle.length > 3) {
+      console.log("Data Valid");
+      return true;
+    } else {
+      videoTitle.classList?.remove("form__input--error");
+    }
+  }
+  function isDescriptionValid() {
+    if (typeof videoDescription === "string" && videoDescription.length > 5) {
+      videoDescription.classList?.remove("form__input--error");
+      console.log("Data Valid");
+      return true;
+    } else {
+      videoDescription.classList?.remove("form__input--error");
+    }
+  }
+
+  // check all details in form are filled
+
+  function isFormComplete() {
+    if (!videoTitle || !videoDescription) {
+      console.log("form INCOMPLETE");
+      alert("Video submission form incomplete");
+      return false;
+    } else {
+      videoTitle.classList?.remove("form__input--error");
+      videoDescription.classList?.remove("form__input--error");
+      console.log("form COMPLETE");
+      return true;
+    }
+  }
 
   function handleFormSubmit(event) {
-    // console.log("i run");
     event.preventDefault();
-    alert("Form submitted successfully !");
-    navigate("/");
+    console.log(event.target);
+    const form = event.target;
+
+    // form validation before upload
+    // const videoTitle = form.title;
+    // console.log(videoTitle.value);
+    // const videoDescription = form.description;
+    // console.log(videoDescription.value);
+
+    // isFormComplete();
+    // check
+    // if (
+    //   typeof videoTitle.value === "string" &&
+    //   typeof videoDescription.value === "string"
+    // ) {
+    //   // videoTitle.classList.remove("input__form--error");
+    //   // videoDescription.classList.remove("input__form--error");
+    //   alert("Video uploaded successfully !");
+    //   navigate("/");
+    // }
+    if (isFormComplete()) {
+      console.log("form valid & complete so UPLOADED");
+      alert("Video uploaded successfully !");
+      form.reset();
+      navigate("/");
+    }
   }
 
   return (
@@ -20,9 +93,13 @@ function UploadForm() {
       </label>
 
       <input
-        className="form__input"
+        // className={`form__input`}
+        className={`form__input ${isTitleValid() ? "" : "form__input--error"} `}
         type="text"
         name="title"
+        value={videoTitle}
+        onChange={handleVideoTitle}
+        autoFocus
         placeholder="Add a title to your video"
       />
 
@@ -31,9 +108,14 @@ function UploadForm() {
       </label>
 
       <textarea
-        className="form__textarea"
-        name="description"
+        // className="form__textarea"
+        className={`form__textarea ${
+          isDescriptionValid() ? "" : "form__input--error"
+        } `}
         id="description"
+        name="description"
+        value={videoDescription}
+        onChange={handleVideoDescription}
         cols="30"
         rows="3"
         placeholder="Add a description to your video"
