1e129e10ade99f128e9991121e56c71ddb34b6cc
Gaurav <program.drgk@gmail.com>
2024-03-17 01:46:25 -0700

    Post a new comment DONE

--- src/components/NewCommentSection/NewCommentSection.jsx
+++ src/components/NewCommentSection/NewCommentSection.jsx
@@ -3,23 +3,80 @@ import Button from "../Button/Button";
 import UserImage from "../../assets/images/Mohan-muruge.jpg";
 import CommentIcon from "../../assets/icons/add_comment.svg";
 import axios from "axios";
+import { useParams } from "react-router-dom";
+
+import { useEffect, useState } from "react";
 
 function NewCommentSection() {
   // base url & api key for axios call
   const baseUrl = "https://unit-3-project-api-0a5620414506.herokuapp.com";
   const apiKey = "5f0fa1cf-41c3-48d9-9f18-aa53a8502f21";
 
+  const [updatedComments, setUpdatedComments] = useState([]);
+
+  let { videoId } = useParams();
+
   // NOTE event handler to Submit Form - sprint 2
-  // function handleSubmit(event) {
-  //   event.preventDefault();
+  function handleSubmit(event) {
+    event.preventDefault();
 
-  //   const postComment = async () => {
-  //     const response = await axios.post(
-  //       `${baseUrl}/videos/${commentId}/comments`
-  //     );
+    const commentName = "bugs bunny";
+    const comment = event.target.newcomment.value;
 
-  //   };
-  // }
+    // form validation for comment
+    if (!comment) {
+      alert("Cant post empty comment");
+    }
+
+    const newComment = {
+      name: commentName,
+      comment: comment,
+    };
+
+    // post methed to post new user comment
+    const postComment = async (newComment) => {
+      try {
+        const { name, comment } = newComment;
+
+        if (!videoId) {
+          videoId = "84e96018-4022-434e-80bf-000ce4cd12b8";
+        }
+
+        const response = await axios.post(
+          `${baseUrl}/videos/${videoId}/comments?api_key=${apiKey}`,
+          { name, comment }
+        );
+
+        // getDataBack();
+      } catch (error) {
+        console.log("Error Posting Comment");
+      }
+    };
+    postComment(newComment);
+
+    event.target.newcomment.value = "";
+  }
+
+  // function to get comments / updated comments after post
+  useEffect(() => {
+    const getDataBack = async () => {
+      try {
+        const response = await axios.get(
+          `${baseUrl}/videos/${videoId}?api_key=${apiKey}`
+        );
+        const foundData = response.data.comments;
+
+        const sortedData = foundData.sort((a, b) => a.timestamp - b.timestamp);
+
+        setUpdatedComments(sortedData);
+      } catch (error) {
+        console.log("Cant get updated comments");
+      }
+    };
+    if (videoId) {
+      getDataBack();
+    }
+  }, [videoId]);
 
   return (
     <article className="newcomment">
@@ -34,13 +91,10 @@ function NewCommentSection() {
       </div>
 
       <div className="newcomment__comment-container">
-        <form
-          className="newcomment__comment-container"
-          // onSubmit={handleSubmit}
-        >
+        <form className="newcomment__comment-container" onSubmit={handleSubmit}>
           <textarea
             className="newcomment__input"
-            name="newcomment__input"
+            name="newcomment"
             type="textarea"
             rows={4}
             cols={5}
